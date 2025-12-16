696c92799a832a9888fa2541f9fea3d7c614bae9
legoamypond <legoamypond@gmail.com>
2025-12-16 21:55:07 +0000

    CapitalazationFigured(Commit2)

--- madLibs.java
+++ madLibs.java
@@ -4,16 +4,25 @@ public class madLibs {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a name");
-        String name = input.nextLine();
+        String name0 = input.nextLine();
+        String name1 =  name0.substring(0,1);
+        String nameRest = name0.substring(1);
+        String name1Up =  name1.toUpperCase();
+        String name = name1Up + nameRest;
         
+
+        /* 
         System.out.println("Enter a verb");
-        String verb1 = input.nextLine();
+        String verb01 = input.nextLine();
+        String verb1 = verb01.toLowerCase();
 
         System.out.println("Enter a shape");
-        String shape = input.nextLine();
+        String shape01 = input.nextLine();
+        String shape = shape01.toLowerCase();
 
         System.out.println("Enter a food");
         String food = input.nextLine();
+        
 
         System.out.println("Enter a liquid");
         String liquid = input.nextLine();
@@ -44,7 +53,8 @@ public class madLibs {
 
         
         String output = String.format("Let's help %s %s their first cake!\nGet out the %s of cake mix\nGet out one %s, a cup of %s, some vanilla extract, and flour\n%s it all together!\nUse a %s to %s the cake less dense.\nOil the pan\nPour batter in the pan and %s for %s minutes for your %s cake to bake.\nTake the cake out of the %s. Carfel it is %s!\nEnjoy",name,verb1,shape,food,liquid,verb2,noun1,verb3,verb4,num,adjective1,noun2,ajective2); 
-        System.out.println(output);
+        */
+        //System.out.println(output);
         
     }
 
